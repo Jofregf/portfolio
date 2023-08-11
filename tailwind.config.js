8370602ea038c23cfdceb8e7ae18f52f0fb451d3
Guillermo Jofre <jofregf@gmail.com>
2023-08-11 16:22:09 -0300

    cambio color

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,23 +16,23 @@ module.exports = {
             },
 
             colors: {
-                dark: "#6D7006",
-                light: "#f4f4f4",
+                dark: "#010001",
+                light: "#E6EBD8",
                 primary: "#FC0200",
-                primaryDark: "#FF9542",
+                primaryDark: "#FFA74E",
             },
             animation: {
                 "spin-slow": "spin 10s linear infinite",
             },
             backgroundImage: {
-                circularLight: "repeating-radial-gradient(rgba(0, 0, 0, 0.4) 2px, #f4f4f4 5px, #f4f4f4 100px);",
-                circularDark: "repeating-radial-gradient(rgba(255, 255, 255, 0.5) 2px, #6D7006 8px, #6D7006 100px);",
-                circularLightLg: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f4f4f4 5px,#f4f4f4 80px)",
-                circularDarkLg: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#6D7006 8px,#6D7006 80px)",
-                circularLightMd: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f4f4f4 5px,#f4f4f4 60px)",
-                circularDarkMd: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#6D7006 6px,#6D7006 60px)",
-                circularLightSm: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f4f4f4 5px,#f4f4f4 40px)",
-                circularDarkSm: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#6D7006 4px,#6D7006 40px)",
+                circularLight: "repeating-radial-gradient(rgba(0, 0, 0, 0.4) 2px, #E6EBD8 5px, #E6EBD8 100px);",
+                circularDark: "repeating-radial-gradient(rgba(255, 255, 255, 0.5) 2px, #010001 8px, #010001 100px);",
+                circularLightLg: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#E6EBD8 5px,#E6EBD8 80px)",
+                circularDarkLg: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#010001 8px,#010001 80px)",
+                circularLightMd: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#E6EBD8 5px,#E6EBD8 60px)",
+                circularDarkMd: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#010001 6px,#010001 60px)",
+                circularLightSm: "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#E6EBD8 5px,#E6EBD8 40px)",
+                circularDarkSm: "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#010001 4px,#010001 40px)",
             }
         },
         screens: {
